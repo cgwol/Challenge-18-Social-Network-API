1647bd88137c162208d68931c2f251d5b4c61722
cgwol <cgwalterson@icloud.com>
2023-10-23 15:02:44 -0700

    Working User Routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,69 @@
 const router = require('express').Router();
+const { User } = require('../../models/')
 
-router.get('/hello', async (req,res) => {
-    
-    try{
-        res.send('HELLO!').status(200)
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find().select('-__v');
+        res.status(200).json(users)
+
+    } catch (err) {
+        res.status(500).json(err);
     }
-    catch(err){
+});
+
+router.get('/:userID', async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.params.userID }).select('-__v');
+
+        if (!user) {
+            res.status(404).json({ message: "No user with that ID" });
+        }
+
+        res.status(200).json(user);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        res.status(200).json(newUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put('/:userID', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            { _id: req.params.userID },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        );
+
+        if (!user) {
+            res.status(404).json({ message: "No user with that ID" });
+        }
+
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.delete('/:userID', async (req, res) => {
+    try{
+        const user = await User.findByIdAndDelete({ _id: req.params.userID });
+
+        if (!user) {
+            res.status(404).json({ message: "No user with that ID" });
+        }
+
+        res.status(200).json({ message: "User deleted successfully!" });
+
+    }catch(err){
         res.status(500).json(err);
     }
 })
