86ede093ea4bb1e0a6709d67903f39c19d19b4f6
cgwol <cgwalterson@icloud.com>
2023-10-23 19:13:43 -0700

    completed thought routes

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -24,4 +24,61 @@ router.get('/:thoughtID', async (req, res) => {
     }
 })
 
+
+router.post('/', async (req, res) => {
+    try{
+
+        const thought = await Thought.create(req.body);
+        
+        const user = await User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $addToSet: { thoughts: thought._id } },
+            { runValidators: true, new: true }
+        )
+
+        if(!user) {
+            res.status(404).json({ message: "No user with that ID" })
+        }
+
+        res.status(200).json(thought)
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+router.put('/:thoughtID', async (req, res) => {
+    try{
+        const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtID },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+
+        if(!thought) {
+            res.status(404).json({ message: "No thought with that ID" })
+        }
+
+        res.status(200).json(thought)
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
+
+router.delete('/:thoughtID', async (req, res) => {
+    try{
+        const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtID });
+
+        if(!thought) {
+            res.status(404).json({ message: "No thought with that ID" })
+        }
+
+        res.status(200).json({ message: "Thought Successfully Deleted" })
+    }
+    catch(err){
+        res.status(500).json(err);
+    } 
+})
+
 module.exports = router;
